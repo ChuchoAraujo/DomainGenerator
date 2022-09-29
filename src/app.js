3c2354e218f9ea44620f211ca37b8f43bd716ab5
ChuchoAraujo <jeseazuara@gmail.com>
2022-09-29 13:40:52 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,32 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+
+  //step 1 - pegar arreglos
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  //step 2 - Crear variable donde se agregaran las combinaciones
+  let domainComplete = [];
+
+  // step 3 - Crear ciclo
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log("test", noun[k]); // step 4 - agregar combinaciones a la variable vacia
+        domainComplete.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+
+  // step 5 -retornar
+  console.log(domainComplete);
+};
